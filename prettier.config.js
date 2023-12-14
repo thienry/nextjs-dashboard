f84b8c051382b768cbaf7ef76308a57f31af4951
Thiago Moura <thiagomf@motorola.com>
2023-12-14 12:06:34 -0300

    CSS Styling; Optimizing Fonts and Images; Creating Layouts and Pages; Navigating Between Pages;

--- prettier.config.js
+++ prettier.config.js
@@ -2,5 +2,11 @@ const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
   ...styleguide,
+  semi: false,
+  printWidth: 100,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'es5',
+  bracketSameLine: true,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
 };
