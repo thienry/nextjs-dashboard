5a2fc84c5154fbb8fbc5ecef320dcff999431b75
Thiago Moura <thiagomf@motorola.com>
2023-12-15 11:10:48 -0300

    Mutate data

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,22 @@
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data'
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
+import Form from '@/app/ui/invoices/edit-form'
+
+type InvoiceEditProps = { params: { id: string } }
+
+export default async function Page({ params }: InvoiceEditProps) {
+  const id = params.id
+  const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()])
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          { label: 'Edit Invoice', href: `/dashboard/invoices/${id}/edit`, active: true },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  )
+}
