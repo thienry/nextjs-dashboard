1bd93722aca898fc66e324c1d4c7015051937548
Thiago Moura <thiagomf@motorola.com>
2023-12-15 11:23:48 -0300

    handling errors on invoices page

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from 'next/navigation'
+
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data'
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import Form from '@/app/ui/invoices/edit-form'
@@ -7,6 +9,10 @@ type InvoiceEditProps = { params: { id: string } }
 export default async function Page({ params }: InvoiceEditProps) {
   const id = params.id
   const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()])
+
+  if (!invoice) {
+    notFound()
+  }
 
   return (
     <main>
